dd7023aa999e95a3a60ab05ab968dce5ae28fb65
viki xiu <vicky.wu1220@gmail.com>
2011-11-24 15:20:42 +0800

    bakup

--- jquery.retina.js
+++ jquery.retina.js
@@ -1,23 +1,37 @@
 jQuery(function($) {
 	$.fn.retina = function(holder,options){
-		var retina = $(this),
-			holder = $(holder),
+		var retina = $(this), holder;
+		if(holder) { holder = $(holder);} else {holder = retina.parent('div')}
+		
+		var	
 			left = 0,scaleX = 0,
-			top = 0,scaleY = 0;
-			imgW = retina.next('div').width(),
-			imgH = retina.next('div').height(),
+			top = 0,scaleY = 0,
 			offset = { left: holder.offset().left, top: holder.offset().top };
-			//sizes = { retina: { width:190, height:190 }, holder:{ width:500, height:283 } };
-		console.log(retina.next('div').width());
+			
+		
 		var sizes ={
 			retina: { width:retina.width(), height:retina.height() }, 
-			holder:{ width:holder.children('img').width(), height:holder.children('img').height()}
+			oImg:{ width:holder.children('img').width(), height:holder.children('img').height()},// origin image size
+			zImg:{width:retina.next('div').width(),height:retina.next('div').height()}// zoom image size
 			//scale:{ x:(imgW-retina.width())/holder.width(), 
 				//	y:(imgH-retina.height())/holder.height() } 
 		}
 		
+		var retinaPara = function(event){
+			this.holderleft = event.pageX - offset.left;
+			this.holdertop = event.pageY - offset.top; 
+			this.scaleX = (sizes.zImg.width - sizes.retina.width)/sizes.oImg.width;
+			this.scaleY = (sizes.zImg.height - sizes.retina.height)/sizes.oImg.height;
+			
+			return {
+						left: this.holderleft - retina.width()/2,
+						top: this.holdertop - retina.height()/2,
+						backgroundPosition : (this.scaleX*this.holderleft*(-1))+'px '+(this.scaleY*this.holdertop*(-1))+'px'
+					};
+		}
 
 		return this.each(function(){
+		
 			//If options exist, merge them with default settings
 			if (options) {
 				$.extend (sizes, options);
@@ -35,20 +49,20 @@ jQuery(function($) {
 			holder.mousemove(function(e){
 
 				left = (e.pageX-offset.left);
-				top = (e.pageY-offset.top);
+				top = (e.pageY-offset.top); 
 
 			
-				scaleX = (imgW - sizes.retina.width)/sizes.holder.width;
-				scaleY = (imgH - sizes.retina.height)/sizes.holder.height;
+				scaleX = (sizes.zImg.width - sizes.retina.width)/sizes.oImg.width;
+				scaleY = (sizes.zImg.height - sizes.retina.height)/sizes.oImg.height;
 				
-				
+				var movePara = new retinaPara(e); 
 
 				if(retina.is(':not(:animated):hidden')){
 					/* Fixes a bug where the retina div is not shown */
 					holder.trigger('mouseenter');
 				}
 
-				if(left<0 || top<0 || left > sizes.holder.width || top > sizes.holder.height)
+				if(left<0 || top<0 || left > sizes.oImg.width || top > sizes.oImg.height)
 				{
 					/*	If we are out of the bondaries of the
 						holder screenshot, hide the retina div */
@@ -61,15 +75,13 @@ jQuery(function($) {
 
 				/*	Moving the retina div with the mouse
 					(and scrolling the background) */
-
+				
 				retina.css({
-					left				: left - retina.width()/2,
-					top					: top - retina.height()/2,
-					backgroundPosition	: (scaleX*left*(-1))+'px '+(scaleY*top*(-1))+'px'
+					left				: movePara.left,
+					top					: movePara.top,
+					backgroundPosition	: movePara.backgroundPosition
 				});
 				
-
-
 				
 			}).mouseleave(function(){
 				retina.stop(true,true).fadeOut('fast');
@@ -78,21 +90,18 @@ jQuery(function($) {
 			});
 			
 
-			var retinaZoom = 0,intOverallDelta = 0,zoomRadius = 0;;
+			
 				
 			holder.mousewheel(function(objEvent, intDelta){ 
+				var retinaZoom = 0,zoomRadius = 0;
+				var scale = retina.width() * 0.2 * intDelta;
+				
+				retinaZoom = retina.width() + scale;
+				
 				if(objEvent.preventDefault){
 					objEvent.preventDefault();}
-					
-				var scale = retina.width() * 0.2 * intDelta;
-			    /*if (intDelta > 0){
-				   intOverallDelta++;
-				}
-			    else if (intDelta < 0){
-					intOverallDelta--;
-				}*/
-				retinaZoom = retina.width() + scale;
-				if(retinaZoom >= sizes.retina.width*0.5 && retinaZoom <= (imgW-(sizes.holder.width - sizes.retina.width/2)*scaleX)/2){
+				
+				if(retinaZoom >= sizes.retina.width*0.5 && retinaZoom <= (sizes.zImg.width-(sizes.oImg.width - sizes.retina.width/2)*scaleX)/2){
 					retina.width(retinaZoom).height(retinaZoom).css({
 						left : retina.offset().left - offset.left - scale/2,
 						top : retina.offset().top - offset.top - scale/2
